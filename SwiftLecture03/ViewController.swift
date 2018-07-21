e4efc363cfc5bf5f26c592a6d4df13c8bd5f9c32
liyan0707 <728762713@qq.com>
2018-07-21 17:48:36 +0800

    add project

---
+++ SwiftLecture03/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SwiftLecture03
+//
+//  Created by stu1 on 2018/7/21.
+//  Copyright © 2018年 tripim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Hello Swift")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
